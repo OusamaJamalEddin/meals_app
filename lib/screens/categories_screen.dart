523a66026de6a36d9bdfbfaa3e25e5af0bc305a5
OusamaJamalEddin <ossama.jamal.aldien@gmail.com>
2025-02-19 22:01:46 +0300

    MealsScreen was created with its sub neccesary files (havent done the design)

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  /*
+  i can use function in stateless widget, to just change the screen, i am not 
+  updating the UI, i am basically manupilating the widgets stack, where 
+  i pop and push as i wish and the very top widget will always be displayed, 
+  so thats why no statefulwidget is needed in this case.
+  and sense i am CURRENTLY in the CATEGORY screen, and I want to move to mealscreen
+  thats why i have to define the function here !, and then accept it as a parameter
+  in the MealsScreen file.
+  */
+  void _selectCategory(
+      BuildContext context, String categoryId, String categoryTitle) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => MealsScreen(
+          meals: dummyMeals
+              .where((meal) => meal.categories.contains(categoryId))
+              .toList(),
+          title: categoryTitle,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Food Categories",
+          //the exclamation mark is telling flutter that titlelarge is defined (its from the google fonts package)
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+              fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(16),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        //By using .map, i managed to print all widgets from the list.
+        //pay attention that i didnt say children: [], because at the end i wrote toList()
+        children: availableCategories.map(
+          (singleCategory) {
+            return CategoryItem(
+              category: singleCategory,
+              /*
+              WHEN I SELECT (CLICK on A CATEGORY for example italian), 
+              i am passing the informations of that category  
+              to the function where i filter the meals based on the category id 
+              and the meal categories attrbiutes, and also i am passing the 
+              title as well to display it on the appBar title, 
+              ( i can just basically pass the whole category as Category and 
+              then in teh fcuntion i can get what i need, but idk i felt this way is better)
+              */
+              onSelectCategory: () {
+                _selectCategory(
+                    context, singleCategory.id, singleCategory.title);
+              },
+            );
+          },
+        ).toList(),
+        /*
+        OR ->
+        children: [
+          for(final category in availableCategories) {
+            MealItem(category: category);
+          }
+        ]
+        */
+      ),
+    );
+  }
+}
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/models/category.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.category});
+  const MealItem({super.key, required this.title});
 
-  final Category category;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32),
-        gradient: LinearGradient(
-          colors: [
-            //0.55 * 255 withAlpha == withOpacity(0.55), same goes for the other one
-            category.color.withAlpha((0.55 * 255).toInt()),
-            category.color.withAlpha((0.9 * 255).toInt()),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.red,
       ),
-      child: Text(category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              )),
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(8),
+      child: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .bodySmall!
+            .copyWith(color: Theme.of(context).colorScheme.onSurface),
+      ),
     );
   }
 }
