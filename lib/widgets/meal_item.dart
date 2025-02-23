e5256ad6da35d81eba40ec5c38a81599024f089c
OusamaJamalEddin <ossama.jamal.aldien@gmail.com>
2025-02-23 17:45:57 +0300

    MealsDetails screen is done (all left later for a button to make some food faviourate)

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals_details_screen.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -7,15 +9,47 @@ class MealItem extends StatelessWidget {
 
   final Meal meal;
 
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
+  //i can either define this function here or define it somewhere {165 : 4:00}
+  //for example i can define this function in the mealscreen and then pass it to here
+  // (i will need meal and context as paramters)
+  void mealsDetailsScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MealsDetailsScreen(meal: meal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
+        onTap: () {
+          mealsDetailsScreen(context);
+        },
         child: Stack(
           children: [
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
             Positioned(
                 bottom: 0,
@@ -30,13 +64,36 @@ class MealItem extends StatelessWidget {
                       Text(
                         meal.title,
                         maxLines: 2,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: true,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                       const SizedBox(
                         height: 6,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [],
+                        children: [
+                          MealItemTrait(
+                            icon: Icons.schedule,
+                            label: '${meal.duration} min',
+                          ),
+                          const SizedBox(width: 12),
+                          MealItemTrait(
+                            icon: Icons.work,
+                            label: complexityText,
+                          ),
+                          const SizedBox(width: 12),
+                          MealItemTrait(
+                            icon: Icons.attach_money,
+                            label: affordabilityText,
+                          )
+                        ],
                       )
                     ],
                   ),
